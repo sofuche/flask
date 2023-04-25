27fc284601e01bfea7c14d58b5d89bcf62052f5e
sofuche <sofya.chetverukhina@gmail.com>
2023-04-25 09:41:40 +0300

    list

--- mission.py
+++ mission.py
@@ -14,6 +14,26 @@ def training(prof):
     return render_template('training.html', title='Тренировка', prof=prof)
 
 
+@app.route('/list_prof/<list>')
+def list_prof(list):
+    lst = ['инженер-исследователь',
+           'пилот',
+           'строитель',
+           'экзобиолог',
+           'врач',
+           'инженер по терраформированию',
+           'климатолог',
+           'специалист по радиационной защите',
+           'астрогеолог',
+           'гляциолог',
+           'инженер жизнеобеспечения',
+           'метеоролог',
+           'оператор марсохода',
+           'киберинженер',
+           'штурман']
+    return render_template('list.html', title='Список профессий', list=list, lst=lst)
+
+
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
