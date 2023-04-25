0a8225425ecda52cd85e82603931a232f2c35028
sofuche <sofya.chetverukhina@gmail.com>
2023-04-25 09:01:15 +0300

    training

---
+++ mission.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('base.html', title='Заготовка')
+
+
+@app.route('/training/<prof>')
+def training(prof):
+    return render_template('training.html', title='Тренировка', prof=prof)
+
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
